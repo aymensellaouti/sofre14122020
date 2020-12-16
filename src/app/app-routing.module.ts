7751d6a2068aac9e1bba8ae36514fcfeac253322
aymen2782 <aymen.sellaouti@gmail.com>
2020-12-16 10:56:19 +0100

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CvComponent } from './cv/cv/cv.component';
+import { WordComponent } from './directives/word/word.component';
+import { ColorComponent } from './pages/color/color.component';
+import { TodoComponent } from './todo/todo.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cv', component: CvComponent },
+  { path: 'todo', component: TodoComponent },
+  { path: 'color', component: ColorComponent },
+  { path: 'word', component: WordComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
